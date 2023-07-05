81fc80f50a9ca1638e776774f4d3273639e605c4
Rohit Solia <rohit.solia@gmail.com>
2023-07-05 18:21:55 +0530

    Sync code with esMET

--- contracts/interface/IRewards.sol
+++ contracts/interface/IRewards.sol
@@ -3,26 +3,17 @@
 pragma solidity 0.8.9;
 
 interface IRewards {
-    function addRewardToken(
-        address rewardsToken_,
-        address distributor_,
-        bool isBoosted_
-    ) external;
+    function addRewardToken(address rewardsToken_, address distributor_, bool isBoosted_) external;
 
     function claimRewards(address account_) external;
 
-    function claimableRewards(address account_)
-        external
-        view
-        returns (address[] memory rewardTokens_, uint256[] memory claimableAmounts_);
+    function claimableRewards(
+        address account_
+    ) external view returns (address[] memory rewardTokens_, uint256[] memory claimableAmounts_);
 
     function dripRewardAmount(address rewardToken_, uint256 rewardAmount_) external;
 
-    function setRewardDistributorApproval(
-        address rewardsToken_,
-        address distributor_,
-        bool approved_
-    ) external;
+    function setRewardDistributorApproval(address rewardsToken_, address distributor_, bool approved_) external;
 
     function updateReward(address account_) external;
 
